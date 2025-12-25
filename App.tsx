2f5473ca98d5bbb4e7d1aacec995ede01abace14
hasanmehediii <mehedi200105075@gmail.com>
2025-12-26 03:07:08 +0600

    fix: configure project for gh-pages deployment

--- App.tsx
+++ App.tsx
@@ -7,14 +7,11 @@ import Skills from './components/Sections/Skills';
 import Projects from './components/Sections/Projects';
 import Contact from './components/Sections/Contact';
 import Footer from './components/Layout/Footer';
-import MatrixBackground from './components/UI/MatrixBackground';
 import Reveal from './components/UI/Reveal';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-cyber-black text-gray-300 selection:bg-cyber-primary selection:text-black overflow-x-hidden">
-      {/* Background Effect */}
-      <MatrixBackground />
       
       {/* Navigation */}
       <Navbar />
